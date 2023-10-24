85404c9341a90e384d9e54e00f7acb219d29b5da
Peterson Ananias <peterson.anania@outlook.com>
2023-10-23 23:06:59 -0300

    Configurando Banco de dados (MySQL)

---
+++ src/cap15/FabricaConexao/Main.java
@@ -0,0 +1,28 @@
+package cap15.FabricaConexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String usuario = "root";
+        String senha = "root";
+        String URL_DB_IMPACTA = "jdbc:mysql://localhost:3306/db_impacta?useTimezone=true&serverTimezone=UTC";
+
+        try {
+            Connection connection = DriverManager.getConnection(usuario, senha, URL_DB_IMPACTA);
+        } catch (SQLException e) {
+            e.printStackTrace();
+//        } finally {
+//            try {
+//                connection.close();
+//            }
+//        } catch(SQLException e){
+//            e.printStackTrace();
+//        }
+        }
+    }
+}
+
